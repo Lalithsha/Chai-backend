1ea3f4f85c9cd19be4238a886a6a1646ea90a047
Lalith Sharma <lalithsharma989@gmail.com>
2024-05-10 01:37:18 +0530

    Add Structure Changes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Controllers -> Functionality
+
+// middlewares -> code to run in-between -> agar koii request aye aur server pe jane ke pehle mai koii checking lagana chahta
+// hu toh mai middle ware mai karta hu. -> example cookies.
+
+// utils -> utilities -> file upload utility example: profile, videos.
+// mail utility , token utility. -> jo functionlity jo baar baar repeat hoti hai kiyu na usse ek file folder mai
+// rakh diya jaye aur uska naam Utilities naam rakh diya jaye 

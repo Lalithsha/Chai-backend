65c522ef9f01c36d23d1bb7ef423753db5cce95f
Lalith Sharma <lalithsharma989@gmail.com>
2024-05-13 19:21:28 +0530

    File Upload Through Multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,35 @@
+import multer from "multer"
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.originalname)
+        console.log(file) // Trying console.log
+    }
+})
+
+export const upload = multer({
+    storage: storage
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
